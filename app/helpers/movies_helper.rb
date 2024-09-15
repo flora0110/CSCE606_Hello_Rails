c8f8529aaca734305c819634ef38e6dbbacfff9a
王傳馨 <flora@wangchuanxindeMacBook-Air.local>
2024-09-15 00:35:09 -0500

    sort function

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
-end
+    def toggle_sort_direction(column)
+      if params[:sort] == column
+        params[:direction] == "asc" ? "desc" : "asc"
+      else
+        "asc"
+      end
+    end
+  end
